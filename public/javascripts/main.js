c0400db2121089fc90d71e5dde1e86219bb8524a
Tom Streller <dusklykan@yahoo.co.uk>
2012-08-23 00:28:19 +0200

    compiled assets

---
+++ public/javascripts/main.js
@@ -0,0 +1,82 @@
+(function() {
+
+  $(function() {
+    var $canvas, $doc, $win, clients, ctx, cursors, drawLine, id, instructions, lastEmit, prev, socket;
+    if (!('getContext' in document.createElement('canvas'))) {
+      return alert('Seems like your browser doesn\'t support an HTML5 canvas!');
+    } else {
+      $doc = $(document);
+      $win = $(window);
+      $canvas = $('#paper');
+      ctx = $canvas[0].getContext('2d');
+      instructions = $('#instructions');
+      id = Math.round($.now() * Math.random());
+      window.drawing = false;
+      clients = [];
+      cursors = [];
+      socket = io.connect();
+      socket.on('moving', function(data) {
+        if (!(data.id in clients)) {
+          cursors[data.id] = ($('<div/>', {
+            "class": 'cursor'
+          })).appendTo('#cursors');
+        }
+        cursors[data.id].css({
+          'left': data.x,
+          'top': data.y
+        });
+        if (data.drawing && clients[data.id]) {
+          drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
+        }
+        clients[data.id] = data;
+        return clients[data.id].updated = $.now();
+      });
+      prev = {};
+      $canvas.on('mousedown', function(e) {
+        e.preventDefault();
+        window.drawing = true;
+        prev.x = e.pageX;
+        prev.y = e.pageY;
+        instructions.fadeOut();
+        return false;
+      });
+      $doc.bind('mouseup mouseleave', function() {
+        return window.drawing = false;
+      });
+      lastEmit = $.now();
+      $doc.on('mousemove', function(e) {
+        if ($.now() - lastEmit > 30) {
+          socket.emit('mousemove', {
+            x: e.pageX,
+            y: e.pageY,
+            drawing: drawing,
+            id: id
+          });
+          lastEmit = $.now();
+        }
+        if (drawing) {
+          drawLine(prev.x, prev.y, e.pageX, e.pageY);
+          prev.x = e.pageX;
+          return prev.y = e.pageY;
+        }
+      });
+      setInterval((function() {
+        var ident;
+        for (ident in clients) {
+          if ($.now() - clients[ident].updated > 10000) {
+            cursors[ident].remove();
+            delete clients[ident];
+            delete cursors[ident];
+          }
+        }
+        return null;
+      }), 10000);
+      return drawLine = function(fromx, fromy, tox, toy) {
+        ctx.moveTo(fromx, fromy);
+        ctx.lineTo(tox, toy);
+        return ctx.stroke();
+      };
+    }
+  });
+
+}).call(this);
